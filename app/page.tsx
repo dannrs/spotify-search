eb0717ceeaa43c41bd9b6a0d011a01c29c26eac2
dannrs <danniramdhani@proton.me>
2023-08-21 16:54:14 +0700

    Change next link to default <a> tag

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,10 @@ import {
 import JaroWinkler from '@/lib/jaro'
 import { Result } from '@/lib/types'
 import { fetcher } from '@/lib/utils'
-import Link from 'next/link'
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState<string>('')
-  const [searchType, setSearchType] = useState<string>('artist')
+  const [searchType, setSearchType] = useState<string>('track')
   const { data: searchResults, error } = useSWR(
     searchQuery ? `/api/search?q=${searchQuery}&type=${searchType}` : null,
     fetcher
@@ -35,28 +34,36 @@ export default function Home() {
       )
     }))
     .sort((a: Result, b: Result) => b.similarity - a.similarity)
-    .map(({ name, image, url, type }: Result) => ({ name, image, url, type }))
+    .map(({ name, image, uri, type }: Result) => ({ name, image, uri, type }))
 
   return (
-    <main className='container flex flex-col items-center justify-center gap-4 md:max-w-[60rem] pt-4'>
+    <main className='container flex flex-col items-center justify-center gap-4 pt-4 md:max-w-[60rem]'>
       <h1 className='text-xl font-semibold'>Spotify Search</h1>
       <div className='flex items-center justify-center gap-2'>
         <Input
           type='text'
           placeholder='Search...'
           value={searchQuery}
-          className='w-[19.5rem] md:w-[30rem]'
+          className='w-[16.5rem] md:w-[30rem]'
           onChange={e => setSearchQuery(e.target.value)}
         />
-        <Select defaultValue='artist' onValueChange={setSearchType}>
+        <Select defaultValue='track' onValueChange={setSearchType}>
           <SelectTrigger className='w-24 md:w-32'>
             <SelectValue placeholder='Select a type' />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value='album'>Album</SelectItem>
-            <SelectItem value='artist'>Artist</SelectItem>
-            <SelectItem value='playlist'>Playlist</SelectItem>
-            <SelectItem value='track'>Track</SelectItem>
+            <SelectItem value='album' onClick={e => e.stopPropagation()}>
+              Album
+            </SelectItem>
+            <SelectItem value='artist' onClick={e => e.stopPropagation()}>
+              Artist
+            </SelectItem>
+            <SelectItem value='playlist' onClick={e => e.stopPropagation()}>
+              Playlist
+            </SelectItem>
+            <SelectItem value='track' onClick={e => e.stopPropagation()}>
+              Track
+            </SelectItem>
           </SelectContent>
         </Select>
       </div>
@@ -65,7 +72,7 @@ export default function Home() {
           {!sortedResults ? (
             <div>Loading...</div>
           ) : (
-            <div className='grid grid-cols-2 gap-4 md:grid-cols-4 pb-8'>
+            <div className='grid grid-cols-2 gap-4 pb-8 md:grid-cols-4'>
               {sortedResults.map((result: Result, index: number) => (
                 <div
                   key={index}
@@ -81,13 +88,14 @@ export default function Home() {
                   <p className='text-foreground/80'>
                     {result.type.charAt(0).toUpperCase() + result.type.slice(1)}
                   </p>
-                  <Link
-                    href={result.url}
+                  <a
+                    href={result.uri}
                     target='_blank'
                     className='absolute inset-0'
+                    onClick={e => e.currentTarget.blur()}
                   >
                     <span className='sr-only'>Open on Spotify</span>
-                  </Link>
+                  </a>
                 </div>
               ))}
             </div>
